4ca58f48ab4bd6b00bae4b9fb1d482f7992b2b0c
Camilo <camilobarrosr@gmail.com>
2022-07-15 03:07:33 -0500

    FirstCommit: Version stable

---
+++ src/GiphyApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BarSearch } from './components/BarSearch'
+
+/* KEY_API: yNH5ySOiYmsZrdM4uykIkbhYE0ffoIcJ */
+
+export const GiphyApp = () => {
+  return (
+    <div>
+      <h1><span id='h1-letter'>G</span>iphy<span id='h1-letter'>A</span>pp</h1>
+      <BarSearch />
+    </div>
+  )
+}
